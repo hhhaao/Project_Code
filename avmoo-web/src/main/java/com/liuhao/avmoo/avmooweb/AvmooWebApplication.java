51f9a3ebbbbee5449d95a1c5ec656a7923f3b8b2
hhhaao <liuhhhaao@163.com>
2018-03-12 00:30:25 +0800

    提交avmoo项目代码

---
+++ avmoo-web/src/main/java/com/liuhao/avmoo/avmooweb/AvmooWebApplication.java
@@ -0,0 +1,12 @@
+package com.liuhao.avmoo.avmooweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvmooWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvmooWebApplication.class, args);
+	}
+}
