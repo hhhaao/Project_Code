764c09801b4e8e1417651f9e8fba741431e53f1e
hhhaao <liuhhhaao@163.com>
2018-03-20 01:19:06 +0800

    Eureka注册8761

--- avmoo-eureka/src/main/java/com/liuhao/avmoo/eureka/AvmooEurekaApplication.java
+++ avmoo-eureka/src/main/java/com/liuhao/avmoo/eureka/AvmooEurekaApplication.java
@@ -2,8 +2,10 @@ package com.liuhao.avmoo.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AvmooEurekaApplication {
 
 	public static void main(String[] args) {
