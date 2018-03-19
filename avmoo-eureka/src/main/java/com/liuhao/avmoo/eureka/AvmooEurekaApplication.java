98e7da37dee1c06316b3dfbf394130a743833ec3
hhhaao <liuhhhaao@163.com>
2018-03-19 19:36:03 +0800

    提交Eureka工程

---
+++ avmoo-eureka/src/main/java/com/liuhao/avmoo/eureka/AvmooEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.liuhao.avmoo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvmooEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvmooEurekaApplication.class, args);
+	}
+}
